0bcd7a195cbcac5cfaae5ac319d732c0290ff1cf
Luan Ribeiro <139171433+LuanDevJR@users.noreply.github.com>
2025-02-14 11:44:50 -0300

    Add files via upload

---
+++ ordemDosProgramas.py
@@ -0,0 +1,14 @@
+""" Ordem dos Programas """
+# Por padrão, Python executa o código linha por linha, de cima para baixo.
+
+faturamento = 1000
+custo = 500
+
+
+lucro = faturamento - custo
+print(lucro)
+
+
+custo = 700
+lucro = faturamento - lucro
+print(lucro)
