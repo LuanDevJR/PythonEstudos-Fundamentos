f0996dfd584434008f3f08dcec41486ffcd4d7da
Luan Ribeiro <139171433+LuanDevJR@users.noreply.github.com>
2025-02-14 11:45:21 -0300

    Add files via upload

---
+++ tiposdeVariaveis.py
@@ -0,0 +1,27 @@
+""" Tipos de Variáveis em Python """
+
+# Uma variável é um objeto, tudo é objeto em Python
+
+"""
+int -> Inteiro
+float -> Números com casas decimais (ponto flutuante)
+str -> Texto
+bool ou booleano -> True ou False
+"""
+
+# Variáveis em Python não são declaradas explicitamente, mas isso não significa que devemos ficar mudando-as o tempo todo
+
+# Cuidado com nomes restritos para a linguagem
+
+
+faturamento = 1000
+print(type(faturamento))
+
+faturamento = 1000.00
+print(type(faturamento))
+
+faturamento = '1000.00'
+print(type(faturamento))
+
+luanEstudando = True
+print(type(luanEstudando))
